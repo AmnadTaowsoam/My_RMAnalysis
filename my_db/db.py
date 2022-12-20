baf1a10bb90ce83f6c22f005fb7b064f8b24ebc3
AmnadTaowsoam <amnadtaow@gmail.com>
2022-12-20 15:53:17 +0700

    Add file to Project

---
+++ my_db/db.py
@@ -0,0 +1,27 @@
+import sys
+import pyodbc
+import configparser
+import pandas as pd
+import datetime
+
+class FpqfDB():
+    parser = configparser.ConfigParser()
+    parser.read('fpqf.ini')
+    SQL_driver = parser['myserv']['SQL_driver']
+    Server = parser['myserv']['Server']
+    Database = parser['myserv']['Database']
+    uid = parser['myserv']['uid']
+    pwd = parser['myserv']['pwd']
+
+    def __init__(self):
+        con_string = f'Driver={self.SQL_driver};Server={self.Server};Database={self.Database};UID={self.uid};pwd={self.pwd}'
+        # con_string = f'Driver={self.SQL_driver};Server={self.Server};Database={self.Database};Trusted_Connection=yes;'
+        try:
+            self.conn = pyodbc.connect(con_string)
+        except Exception as e:
+            print(e)
+            print('Task is terminate')
+            sys.exit()
+        else:
+            self.cursor = self.conn.cursor()
+            print('Conection successfully','(',datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),')')

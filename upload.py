5fe40249c6ef611af0183a4dab64e74f0926b61f
AmnadTaowsoam <amnadtaow@gmail.com>
2022-12-23 18:25:40 +0700

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,19 @@
+import datetime
+from my_function.rmanalysis import Processing
+from my_db.dbb import RMDBB
+
+dbb = RMDBB()
+pc = Processing()
+
+if __name__=="__main__":
+        try:
+            pc.update_rmanalysis()
+            print('Upload rmanalysis to database successfully','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')
+        except:
+            print('Upload rmanalysis to database error','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')
+            
+        try:
+            dbb.truncate_rmbuffer_tbl()
+            print('truncate_rmbuffer_tbl database successfully','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')
+        except:
+            print('truncate_rmbuffer_tbl database error','(',datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),')')

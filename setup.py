7018b565d234c239dae729458169490c31d8191b
AmnadTaowsoam <amnadtaow@gmail.com>
2022-12-23 18:24:43 +0700

    add dbb

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from my_db.db import RMDB
+from my_db.dbb import RMDBB
 
 db = RMDB()
-
+dbb = RMDBB()
 if __name__=="__main__":
         try:
             db.create_rmanalysis_tbl()
+            dbb.create_rmbuffer_tbl()
         except:
             print('create_copellet_tbl error')
